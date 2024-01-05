3da9220c5735a4b8670aed9ceca3b5701a13c3d2
Emmalex22 <emmanuelpelumi22@gmail.com>
2024-01-04 16:24:26 -0800

    upated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,13 +2,12 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-    from calculator_1 import add, subtract, multiply, divide
+    from calculator_1 import add, sub, mul, div
     answer_add = add(a, b)
-    answer_subtract = subtrct(a, b)
-    answer_multiply = multiply(a, b)
-    answer_divide = divide(a, b)
+    answer_sub = sub(a, b)
+    answer_mul = mul(a, b)
+    answer_div = div(a, b)
     print("Addition is:", answer_add)
-    print("Subtraction is:", answer_subtract)
-    print("Multiplication is:", answer_multiply)
-    print("Division is:", answer_divide)
-    
+    print("Subtraction is:", answer_sub)
+    print("Multiplication is:", answer_mul)
+    print("Division is:", answer_div)
