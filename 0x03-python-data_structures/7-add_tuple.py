ec1a1eb3951827d2f2c7dc9f768af09635d46190
Emmalex22 <emmanuelpelumi22@gmail.com>
2024-01-08 18:47:42 -0800

    updated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     extended_tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
     extended_tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
-    result_tuple = (extended_tuple_a[0] + extended_tuple_b[0], extended_tuple_a[1] + extended_tuple_b[1])
+    sum_first_elements = extended_tuple_a[0] + extended_tuple_b[0]
+    sum_second_elements = extended_tuple_a[1] + extended_tuple_b[1]
+    result_tuple = (sum_first_elements, sum_second_elements)
     return result_tuple
