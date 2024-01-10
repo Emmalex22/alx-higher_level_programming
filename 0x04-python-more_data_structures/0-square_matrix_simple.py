2664bdc74d757ae205939b36282ed0cb8deba139
Emmalex22 <emmanuelpelumi22@gmail.com>
2024-01-10 14:16:20 -0800

    updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         neo_row = []
         for elements in row:
-            neo_row.append(element ** 2)
+            neo_row.append(elements ** 2)
         squared_matrix.append(neo_row)
     return squared_matrix
