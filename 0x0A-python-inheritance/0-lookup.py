346ee419a35e23004289511e294379d28fedec52
Emmalex22 <emmanuelpelumi22@gmail.com>
2024-02-05 20:12:20 -0800

    updated

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
-    """
-    Get a list of public attributes and methods of an object.
-
-    Args:
-    - obj: The object for which to retrieve attributes and methods.
-
-    Returns:
-    - A list containing the public attributes and methods of the object.
-    """
-    # Get all attributes and methods using dir() function
-    attributes_and_methods = dir(obj)
-
-    # Filter out any private or special attributes/methods
-    filtered_attributes_and_methods = [
-        attr for attr in attributes_and_methods if not attr.startswith("__")
-    ]
-    return filtered_attributes_and_methods
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
